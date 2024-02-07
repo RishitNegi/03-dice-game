4d9d54f1178484451708536bb65a5468671b8282
Rishit Negi <rishitnegi2001@gmail.com>
2024-02-07 18:25:13 +0530

    "Changed the name of "
    	renamed:    src/Dice/Dices.png -> src/Images/Dices.png renamed:
        src/Dice/Logo.png -> src/Images/Logo.png renamed:
        src/Dice/dice_1.png -> src/Images/dice_1.png renamed:
        src/Dice/dice_2.png -> src/Images/dice_2.png renamed:
        src/Dice/dice_3.png -> src/Images/dice_3.png renamed:
        src/Dice/dice_4.png -> src/Images/dice_4.png renamed:
        src/Dice/dice_5.png -> src/Images/dice_5.png renamed:
        src/Dice/dice_6.png -> src/Images/dice_6.png modified:
        src/components/RollDice.jsx modified: src/components/StartGame.jsx

--- src/components/StartGame.jsx
+++ src/components/StartGame.jsx
@@ -5,7 +5,7 @@ const StartGame = ({toggle}) => {
   return (
     <>
       <Container>
-        <img src="src/Dice/Dices.png" alt="" />
+        <img src="src/Images/Dices.png" alt="" />
         <div className="content">
           <h1>DICE GAME</h1>
           <Button onClick={toggle}>Play Now</Button>
