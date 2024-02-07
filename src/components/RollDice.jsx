9a1f40edeb36120538f38c7555cd0b5964506b60
Rishit Negi <rishitnegi2001@gmail.com>
2024-02-07 18:35:43 +0530

    undid the path change

--- src/components/RollDice.jsx
+++ src/components/RollDice.jsx
@@ -26,7 +26,7 @@ const RollDice = ({ CurrentDice, rollDice, SelectedNumber }) => {
               ? notify()
               : rollDice()
           }
-          src={`https://github.com/RishitNegi/03-dice-game/tree/main/src/Images/dice_${CurrentDice}.png`}
+          src={`src/Images/dice-${CurrentDice}.png`}
           alt={`Dice ${CurrentDice}`}
         />
         <p>Click on dice to roll </p>
